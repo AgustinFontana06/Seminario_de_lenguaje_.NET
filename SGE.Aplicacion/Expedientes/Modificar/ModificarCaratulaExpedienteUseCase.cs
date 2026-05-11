e6bb09671623043d948cc68bbc296966a1fbeb19
Agustin <agus.fonta06@gmail.com>
2026-05-11 17:50:39 -0300

    cambios en aplicacion e infraestructura carpeta Expediente

--- SGE.Aplicacion/Expedientes/Modificar/ModificarCaratulaExpedienteUseCase.cs
+++ SGE.Aplicacion/Expedientes/Modificar/ModificarCaratulaExpedienteUseCase.cs
@@ -5,6 +5,6 @@ public class ModificarCaratulExpedienteUseCase(IExpedienteRepository repositorio
 {
     public ModificarCaratulaExpedienteResponse Ejecutar(ModificarCaratulaExpedienteRequest request)
     {
-        
+        //hola
     }
 }
