335a4f7036ec1e76831cd6e7e01182a8663376e6
Agustin <agus.fonta06@gmail.com>
2026-06-19 17:48:59 -0300

    modifique registrar use case

--- SGE.Aplicacion/Usuarios/Registrar/RegistrarUsuarioUseCase.cs
+++ SGE.Aplicacion/Usuarios/Registrar/RegistrarUsuarioUseCase.cs
@@ -4,27 +4,20 @@ using SGE.Aplicacion.Abstracciones;
 using SGE.Aplicacion.Excepciones;
 using SGE.Dominio.Usuario;
 
-public class RegistrarUsuarioUseCase(IUnidadDeTrabajo unidadDeTrabajo, IUsuarioRepository usuarioRepository)
+public class RegistrarUsuarioUseCase(IUnidadDeTrabajo udt, IUsuarioRepository usuarioRepository)
 {
     public RegistrarUsuarioResponse Ejecutar(RegistrarUsuarioRequest request)
     {
         //1.Validar que el usuario no existe
-        if(usuarioRepository.ExisteUsuario(request.email))
+        if(usuarioRepository.ExisteUsuario(request.id))
         {
             throw new EntidadNoEncontradaException("El usuario ya existe");
         }
-        var partesEmail = request.email.Split('@');
-        if (partesEmail.Length != 2)
-        {
-            // Lanzamos DominioException para que nuestro futuro filtro devuelva un HTTP 400
-            throw new ApplicationException("El formato del email no es válido.");
-        }
 
         //2.Crear el usuario
-        var direccionEmail = new DireccionEmail(partesEmail[0], partesEmail[1]);
-        var usuario = new Usuario(request.nombre, direccionEmail,  request.password);
-        usuarioRepository.Agregar(usuario);
-        unidadDeTrabajo.GuardarCambios();
+        var usuario = new Usuario(request.nombre, request.email, request.password);
+        usuarioRepository.AgregarUsuario(usuario);
+        udt.GuardarCambios();
         return new RegistrarUsuarioResponse(usuario.Id);
     }
 }
