644ba3624cd98f852fa7b859e60e70a8bed25e3c
ManuelSepulveda <manusombra11@gmail.com>
2026-06-19 21:19:04 -0300

    mergeo2

--- SGE.Aplicacion/Usuarios/Login/LoginUsuarioRequest.cs
+++ SGE.Aplicacion/Usuarios/Login/LoginUsuarioRequest.cs
@@ -3,4 +3,4 @@ using SGE.Dominio.Usuario;
 namespace SGE.Aplicacion.Usuarios;
 
 
-public record LoginUsuarioRequest(DireccionEmail email, string password){}
+public record LoginUsuarioRequest(string email, string password){}
